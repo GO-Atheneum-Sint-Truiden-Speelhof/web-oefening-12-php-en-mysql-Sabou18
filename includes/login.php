6e433f1a0906fdc3812c0172e26ef1887093a9fa
Sabou18 <abou.sal@atheneumsinttruiden.be>
2024-11-07 16:14:18 +0100

    inschrijvingen, login, logout gemaakt

---
+++ includes/login.php
@@ -0,0 +1,19 @@
+<div class="jumbotron">
+    <h1 class="display-4">Tijd om in te loggen!!!</h1>
+</div>
+
+<div class="col-lg-12">
+    <div class="col">
+        <div class="form-group row">
+            <label for="naam">gebruikersnaam</label><br>
+            <p><input type="text" class="form-control" name="username" size="46"></p>
+        </div>
+        
+        <div class="form-group row">
+            <label for="password">wachtwoord</label><br>
+            <p><input type="text" class="form-control" name="password" size="46"></p>
+        </div>
+
+        <input type="submit" value="login">
+    </div>
+</div>
